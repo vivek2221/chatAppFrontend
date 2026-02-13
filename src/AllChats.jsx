46ca70423b9b6e5e8f891c4c6eaaf529bf776dd1
vivek shekhawat <vshekhawat136@gmail.com>
2026-02-13 12:28:21 +0530

    making logout work

--- src/AllChats.jsx
+++ src/AllChats.jsx
@@ -101,6 +101,10 @@ function AllChats(){
             }
         }
         else if(msgData.kindOf==='reLogin'){
+            fetch(`${import.meta.env.VITE_URL_SERVER}`,{
+                method:'DELETE',
+                credentials:'include'
+            })
             navigate('/')
         }
     }
